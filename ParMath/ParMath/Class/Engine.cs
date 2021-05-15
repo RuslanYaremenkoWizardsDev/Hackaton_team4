0c83dbf81e798c49504f5146ac128b2248e64b94
Данил Новиков <finaut477@gmail.com>
2021-05-15 23:24:04 +0300

    add Class: Engine and write property Dictionary which serves to store current users, and add method for search, added new User

---
+++ ParMath/ParMath/Class/Engine.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ParMath.Class
+{
+    public class Engine
+    {
+        private Dictionary<string, string> allUser = new Dictionary<string, string>();
+
+        public void AddToDictionary(User currentUser)
+        {
+            allUser.Add(currentUser.Username, currentUser.Password);
+        }
+
+        public void FindInDictionary(string username, string password)
+        {
+            foreach (KeyValuePair<string, string> entry in allUser)
+            {
+                if (username == entry.Key)
+                {
+                    if (password == entry.Value)
+                    {
+                        break;
+                    }
+                    throw new ArgumentException("Error: Password not find");
+                }
+                throw new AggregateException("Error: UserName not find");
+            }
+        }
+    }
+}

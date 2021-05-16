4fed54c40778bd87cef230cf03656618ae2c7276
Alexey <nhevgevgev@gmail.com>
2021-05-16 23:14:50 +0300

    Add CupGridForm
    Create cup grid without lines

--- ParMath/ParMath/Program.cs
+++ ParMath/ParMath/Program.cs
@@ -16,7 +16,7 @@ namespace ParMath
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new AuthorizationForm());
+            Application.Run(new CupGridForm());
 
         }
     }

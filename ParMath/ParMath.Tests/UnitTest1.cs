fe2631c86f9ebc3139fad7dea519c3c7527f8030
Danil Novikov <finaut477@gmail.com>
2021-05-16 21:03:14 +0300

    add test method Engine: FindUsers, UniquelyUser, Seeds, AddOneUser

--- ParMath/ParMath.Tests/UnitTest1.cs
+++ ParMath/ParMath.Tests/UnitTest1.cs
@@ -8,24 +8,42 @@ namespace ParMath.Tests
     {
         private Engine _engine;
         private User _firstUser;
+        private User _invalideUser; 
 
         [SetUp]
         public void Setup()
         {
             _engine = new Engine();
             _firstUser = User.CreateUser("Shaitan", "111111");
+            _invalideUser = User.CreateUser("Sh", "000");
+            _engine.Users.Add(_firstUser);
         }
 
         [Test]
-        public void TestAddToDictionary()
+        public void TestAddOneUser()
         {
-
+            Assert.AreEqual(1, _engine.Users.Count);
         }
 
-        [TestCase]
-        public void FindInDictionaryUser()
+        [Test]
+        public void TestSeeds()
         {
-            
+            _engine.Seeds(7);
+            Assert.AreEqual(8, _engine.Users.Count);
+        }
+
+        [Test]
+        public void TestFindUsers()
+        {
+            Assert.AreEqual(true, _engine.FindUser("Shaitan", "111111"));
+            Assert.AreEqual(false, _engine.FindUser("Demon", "1111111"));
+        }
+
+        [Test]
+        public void TestUniquelyUser()
+        {
+            Assert.AreEqual(false, _engine.UniquelyUser("Shaitan"));
+            Assert.AreEqual(true, _engine.UniquelyUser("Demon"));
         }
     }
 }
